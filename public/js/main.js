d34c548d07c9ab9f646c0c156bf942109e9f8582
Brian Jenkins <brianjenkins94@gmail.com>
2020-06-21 11:25:12 -0400

    Colored pins incoming

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,11 @@
 
 const markers = [];
 
+let openInfoWindow;
+
 function createMarkerOnMap(map, place) {
 	markers.push(new globalThis.google.maps.Marker({
+		//"icon": "",
 		"map": map,
 		"position": place.geometry.location
 	}));
@@ -11,8 +14,24 @@ function createMarkerOnMap(map, place) {
 	globalThis.google.maps.event.addListener(markers[markers.length - 1], "click", function() {
 		const infoWindow = new globalThis.google.maps.InfoWindow();
 
-		infoWindow.setContent(place.name);
+		// Close open window
+
+		if (openInfoWindow !== undefined) {
+			openInfoWindow.close();
+		}
+
+		// Open new window
+
+		infoWindow.setContent(`
+			<p>
+            	${place.name} <br />
+        	</p>
+        	<a href="https://www.google.com/search?q=${place.name.toLowerCase().replace(' ', '+')}+menu" target="_blank">Search for the cheapest item!</a>
+		`);
+
 		infoWindow.open(map, this);
+
+		openInfoWindow = infoWindow;
 	});
 }
 
@@ -24,52 +43,80 @@ globalThis.googleMapsResponseHandler = function() {
 		});
 
 		const map = new globalThis.google.maps.Map(document.getElementById("viewer"), {
-			"zoom": 10,
+			"zoom": 11,
 			"center": location
 		});
 
 		function updateResults() {
-			const service = new globalThis.google.maps.places.PlacesService(map);
+			// Clear old markers
 
-			service.nearbySearch({
-				"bounds": map.getBounds(),
-				"keyword": "free wifi"
-			}, function(results, status) {
-				if (status === globalThis.google.maps.places.PlacesServiceStatus.OK) {
-					const buffer = [];
+			for (const marker of markers) {
+				marker.setMap(null);
+			}
 
-					// Clear old markers
+			const buffer = [];
 
-					for (const marker of markers) {
-						marker.setMap(null);
-					}
-
+			function resultsHandler(results) {
+				return new Promise(function(resolve, reject) {
 					// Add new markers
 
 					for (const result of results) {
-						createMarkerOnMap(map, result);
+						if (result.business_status === "OPERATIONAL") {
+							createMarkerOnMap(map, result);
 
-						buffer.push(`
-							<li>
-								<div class="details">
-									<h3>${result.name}</h3>
-									<ul>
-										<li>${"<i class=\"fa fa-star\"></i>".repeat(result.rating)}</li>
-										<li>${result.user_ratings_total}</li>
-									</ul>
-									<ul>
-										<li>${"<i class=\"fa fa-usd\"></i>".repeat(result.price_level)}</li>
-										<li>${result.types[0]}</li>
-										<li>${result.vicinity}</li>
-									</ul>
-									<p class="description active">${result.business_status === "OPERATIONAL" ? result.opening_hours.open_now : ""}</p>
-								</div>
-							</li>
-						`);
+							buffer.push(`
+								<li>
+									<div class="details">
+										<h3>${result.name}</h3>
+										<ul>
+											<li>${"<i class=\"fa fa-star\"></i>".repeat(result.rating)}</li>
+											<li>${result.user_ratings_total}</li>
+										</ul>
+										<ul>
+											<li>${"<i class=\"fa fa-usd\"></i>".repeat(result.price_level)}</li>
+											<li>${result.types[0][0].toUpperCase() + result.types[0].substring(1)}</li>
+											<li>${result.vicinity}</li>
+										</ul>
+										<p class="description active">${result.opening_hours?.open_now ? "Open now!" : "Not currently open."}</p>
+									</div>
+								</li>
+							`);
+						}
 					}
 
-					document.getElementById("results").innerHTML = buffer.join("");
-				}
+					resolve();
+				});
+			}
+
+			const service = new globalThis.google.maps.places.PlacesService(map);
+
+			Promise.all([
+				new Promise(function(resolve, reject) {
+					service.nearbySearch({
+						"bounds": map.getBounds(),
+						"type": "library"
+					}, async function(results, status) {
+						if (status === globalThis.google.maps.places.PlacesServiceStatus.OK) {
+							await resultsHandler(results);
+
+							resolve();
+						}
+					});
+				}),
+				new Promise(function(resolve, reject) {
+					service.nearbySearch({
+						"bounds": map.getBounds(),
+						"keyword": "free wifi"
+					}, async function(results, status) {
+						if (status === globalThis.google.maps.places.PlacesServiceStatus.OK) {
+							await resultsHandler(results);
+
+							resolve();
+						}
+					});
+				})
+			]).then(function() {
+				document.getElementById("results").innerHTML = buffer.join("");
 			});
 		}
 
@@ -83,13 +130,15 @@ globalThis.googleMapsResponseHandler = function() {
 
 		globalThis.google.maps.event.addListenerOnce(map, "tilesloaded", updateResults);
 
-		globalThis.google.maps.event.addListenerOnce(map, "dragend", async function onCenterChanged() {
-			console.log("Center changed!");
-
+		async function updateMap() {
 			await updateResultsOnIdle();
 
-			globalThis.google.maps.event.addListenerOnce(map, "dragend", onCenterChanged);
-		});
+			globalThis.google.maps.event.addListenerOnce(map, "dragend", updateMap);
+		}
+
+		globalThis.google.maps.event.addListenerOnce(map, "dragend", updateMap);
+
+		//globalThis.google.maps.event.addListenerOnce(map, "zoom_changed", updateMap);
 	}, function(error) {
 		const response = prompt("I see you didn't want to share your location with us. Fair enough. Can we have your ZIP code instead?", "90210");
 
